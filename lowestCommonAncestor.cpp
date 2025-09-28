64e8dbc404a44aecbb6434759034a0492244afcc
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2025-09-28 14:23:43 +0530

    Add files via upload

--- lowestCommonAncestor.cpp
+++ lowestCommonAncestor.cpp
@@ -7,26 +7,19 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL){
             return NULL;
         }
-
-        if(root->val == p->val || root->val == q->val){
-            return root;
-        }
-
-        TreeNode* leftLCA = lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightLCA = lowestCommonAncestor(root->right,p,q);
-
-        if(leftLCA && rightLCA){
-            return root;
-        } else if (leftLCA != NULL){
-            return leftLCA;
+        if(root->val > p->val && root->val > q->val){
+            return lowestCommonAncestor(root->left, p, q);
+        } else if(root->val < p->val && root->val < q->val){
+            return lowestCommonAncestor(root->right, p, q);
         } else {
-            return rightLCA;
+            return root;
         }
     }
 };
